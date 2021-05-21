d6db32fbd3eb7abe874386b1480c1186df7e3d5d
Dem0nZ <cbpskov@gmail.com>
2021-05-21 17:06:46 +0300

    Slider fading

--- src/slider.js
+++ src/slider.js
@@ -4,6 +4,7 @@ const nextBtn = document.querySelector( '#nextSlide' )
 const prevBtn = document.querySelector( '#prevSlide' )
 
 const slideWidth = 375
+const fadedSlideOpacity = 0.2
 
 const createSlide = (slide) => {
     let li = document.createElement('li')
@@ -15,6 +16,7 @@ const createSlide = (slide) => {
 let position = 0
 let firstSlide = createSlide(slides[position])
 let secondSlide = createSlide(slides[position + 1])
+secondSlide.style.opacity = fadedSlideOpacity.toString()
 
 slidesUl.append(firstSlide)
 slidesUl.append(secondSlide)
@@ -33,6 +35,7 @@ const moveNext = () => {
     }
 
     let newSlide = createSlide(slides[nextPosition])
+    newSlide.style.opacity = fadedSlideOpacity.toString()
     slidesUl.append(newSlide)
 
     firstSlide.ontransitionend = () => {
@@ -48,7 +51,9 @@ const moveNext = () => {
         animationInProgress = false
     }
     firstSlide.style.marginLeft = `-${slideWidth}px`
-    firstSlide.style.transition = 'margin-left ease 1s'
+    firstSlide.style.transition = 'margin-left ease 3s'
+    secondSlide.style.opacity = '1.0'
+    secondSlide.style.transition = 'opacity ease 3s'
 }
 
 const movePrev = () => {
@@ -69,6 +74,8 @@ const movePrev = () => {
         secondSlide.remove()
         secondSlide = firstSlide
         firstSlide = newSlide
+        secondSlide.style = `opacity: ${fadedSlideOpacity}`;
+        firstSlide.onanimationend = null
 
         position--
         if (position < 0) {
@@ -76,7 +83,9 @@ const movePrev = () => {
         }
         animationInProgress = false
     }
-    newSlide.style = 'animation: move_prev 1s linear 0s alternate;'
+
+    newSlide.style = 'animation: move_prev 3s ease 0s alternate;'
+    firstSlide.style = 'animation: opacity_prev 3s ease alternate;'
 }
 
 nextBtn.onclick = () => moveNext()
